a95de5fe2b06cf6b271ded7461dbcf9acb017ad6
Mirco De Zorzi <mircodezorzi@protonmail.com>
2019-08-21 20:37:46 +0200

    fixed issue with tree traversal, added documentation

--- README.txt
+++ README.txt
@@ -1,6 +1,5 @@
 xml2json is a tiny (46 sloc), header only library to convert xml documents into nlohmann::json objects.
 
 TODO:
-	- docs
 	- examples
 	- write a wrapper for better ease of use.
--- xml2json.hpp
+++ xml2json.hpp
@@ -2,6 +2,7 @@
 #define XML2JSON_H
 
 #include <string>
+#include <iostream>
 
 #include "rapidxml.hpp"
 #include "json.hpp"
@@ -10,47 +11,46 @@ using json = nlohmann::json;
 inline json xml2json(std::string xml);
 inline json _xml2json(rapidxml::xml_node<> *root);
 
+/**
+ * Recursively traverses the xml tree and converts it into a nlohmann::json object.
+ * TODO remove the #text key if object has no attributes.
+ * TODO possibly reduce the calls to node->name()?
+ */
 json _xml2json(rapidxml::xml_node<> *root)
 {
-
 	std::map<std::string, int> occurrence;
-
 	json j;
 
 	for(rapidxml::xml_node<> *node = root->first_node(); node; node = node->next_sibling()) {
 		occurrence[node->name()] += 1;
 
-		if (occurrence[node->name()] > 1) { /* list of xml nodes */
-			if (occurrence[node->name()] == 2)
-				j = { j }; /* convert existing json node to an array */
-
+		if (occurrence[node->name()] == 2) /* convert the node to a list */
+			j[node->name()] = { j[node->name()] };
+		if (occurrence[node->name()] > 1) { /* in case there's a list of nodes */
 			json n;
-
+			if(node->first_node())
+				n[node->name()] = _xml2json(node);
+			else
+				n["#text"] = node->value();
 			for(rapidxml::xml_attribute<> *attr = node->first_attribute(); attr; attr = attr->next_attribute())
 				n[node->name()][attr->name()] = attr->value();
-
-			if(node->type() == rapidxml::node_data || node->type() == rapidxml::node_cdata)
-				n[node->name()]["@val"] = node->value();
-			else if(node->type() == rapidxml::node_element)
-				n[node->name()]["@val"] = _xml2json(node);
-
-			j.push_back(n);
-
+			j[node->name()].push_back(n[node->name()]);
 		} else { /* normal xml node */
+			if(node->first_node())
+				j[node->name()] = _xml2json(node);
+			else
+				j["#text"] = node->value();
 			for(rapidxml::xml_attribute<> *attr = node->first_attribute(); attr; attr = attr->next_attribute())
 				j[node->name()][attr->name()] = attr->value();
-
-			if(node->type() == rapidxml::node_data || node->type() == rapidxml::node_cdata)
-				j[node->name()]["@val"] = node->value();
-			else if(node->type() == rapidxml::node_element)
-				j[node->name()]["@val"] = _xml2json(node);
 		}
 	}
 
 	return j;
-
 }
 
+/**
+ * Wrapper function for the xml to json conversion.
+ */
 json xml2json(std::string xml)
 {
 	json j;
