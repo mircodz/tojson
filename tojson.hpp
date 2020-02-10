51cc7f3f385fbd13d5a0b59c31753f8c0c328593
Mirco De Zorzi <mircodezorzi@protonmail.com>
2020-02-10 17:57:11 +0100

    feat(tojson): clean up functions
    - Add optional nodiscard attribute
    - Changed documentation styling
    - Consistent uniform initialization
    - Remove output to stderr in case of exception

--- tojson.hpp
+++ tojson.hpp
@@ -1,5 +1,5 @@
-#ifndef TOJSON_H
-#define TOJSON_H
+#ifndef TOJSON_HPP_
+#define TOJSON_HPP_
 
 #include <exception>
 #include <fstream>
@@ -7,10 +7,15 @@
 
 #include <yaml-cpp/yaml.h>
 
-/* header only libraries */
 #include "rapidxml.hpp"
 #include "json.hpp"
 using json = nlohmann::json;
+
+#if __has_cpp_attribute(nodiscard)
+	#define TOJSON_NODISCARD [[nodiscard]]
+#else
+	#define TOJSON_NODISCARD
+#endif
 
 namespace tojson {
 
@@ -24,33 +29,27 @@ class ParseError : public std::exception
 
 namespace detail {
 
-/**
- * Recursively traverses the xml tree and converts it into a nlohmann::json
- * object.  Possibly override the [] operator to automatically access the
- * `#text` element when the result is used as a string.
- */
 inline json xml2json(rapidxml::xml_node<> *root)
 {
-	json j;
-	std::map<std::string, int> occurrence;
+	json j{};
+	std::map<std::string, int> occurrence{};
 
-	/* rapidxml::xml_node<> */
 	for (auto *node = root->first_node(); node; node = node->next_sibling()) {
-		const char *key = node->name();
+		std::string key = node->name();
 		occurrence[key] += 1;
 
-		/* if seen current node, convert it into a list */
+		// if seen current node, convert it into a list
 		if (occurrence[key] == 2)
 			j[key] = { j[key] };
 
-		/* if the node is a sequence */
+		// if the node is a sequence
 		if (occurrence[key] > 1) {
-			json n;
+			json n{};
 			if(node->first_node())
 				n[key] = xml2json(node);
 			else
 				n["#text"] = node->value();
-			/* iterate through the attributes  */
+			// iterate through the attributes
 			for(auto *attr = node->first_attribute(); attr; attr = attr->next_attribute())
 				n[key][attr->name()] = attr->value();
 			j[key].push_back(n[key]);
@@ -60,7 +59,7 @@ inline json xml2json(rapidxml::xml_node<> *root)
 			} else {
 				j["#text"] = node->value();
 			}
-			/* iterate through the attributes  */
+			// iterate through the attributes
 			for (auto *attr = node->first_attribute(); attr; attr = attr->next_attribute())
 				j[key][attr->name()] = attr->value();
 		}
@@ -69,14 +68,9 @@ inline json xml2json(rapidxml::xml_node<> *root)
 	return j;
 }
 
-/**
- * Recursively traverses the yaml tree and converts it into a nlohmann::json
- * object.  As yaml and json files are essentially the same we don't have to do
- * anything special in this function.
- */
 inline json yaml2json(YAML::Node root)
 {
-	json j;
+	json j{};
 
 	switch (root.Type()) {
 	case YAML::NodeType::Null:
@@ -85,7 +79,7 @@ inline json yaml2json(YAML::Node root)
 		return root.as<std::string>();
 	case YAML::NodeType::Sequence:
 		for (auto node : root)
-			j.push_back(yaml2json(node));
+			j.emplace_back(yaml2json(node));
 		break;
 	case YAML::NodeType::Map:
 		for (const auto &it : root) {
@@ -102,75 +96,68 @@ inline json yaml2json(YAML::Node root)
 
 }
 
-/**
- * Wrapper function for xml to nlohmann::json conversion.
- */
-inline json xml2json(std::string str)
+/// \brief Convert XML string to JSON.
+TOJSON_NODISCARD inline json xml2json(const std::string &str)
 {
-	json j;
-	rapidxml::xml_document<> doc;
+	json j{};
+	rapidxml::xml_document<> doc{};
+
 	try {
 		doc.parse<0>(const_cast<char*>(str.data()));
 	} catch (const rapidxml::parse_error &e) {
-		std::cerr << "parse error: " << e.what() << "\n";
-		throw ParseError();
-    } catch (const std::exception &e) {
-		std::cerr << "error: " << e.what() << "\n";
-		return NULL;
+		throw ParseError{};
+	} catch (const std::exception &e) {
+		return {};
 	}
+
 	rapidxml::xml_node<> *root = doc.first_node();
-	if(root)
+	if (root)
 		j[root->name()] = detail::xml2json(root);
+
 	return j;
 }
 
-
-/**
- * Wrapper function for yaml to nlohmann::json conversion.
- */
-inline json yaml2json(std::string_view str)
+/// \brief Convert YAML string to JSON.
+TOJSON_NODISCARD inline json yaml2json(const std::string &str)
 {
-    YAML::Node root;
-    try {
-        root = YAML::Load(std::string(str));
+	YAML::Node root;
+
+	try {
+		root = YAML::Load(str);
 	} catch (const YAML::ParserException &e) {
-		std::cerr << "parse error: " << e.what() << "\n";
-		throw ParseError();
-    } catch (const std::exception& e) {
-		std::cerr << "error: " << e.what() << "\n";
-        return NULL;
-    }
+		throw ParseError{};
+	} catch (const std::exception& e) {
+		return json{};
+	}
+
 	return detail::yaml2json(root);
 }
 
-/**
- * Load a file containing a yaml document into a nlohmann::json object.
- */
-inline json loadyaml(std::string_view filepath)
+/// \brief Load a YAML file to JSON.
+TOJSON_NODISCARD inline json loadyaml(const std::string &filepath)
 {
-    YAML::Node root;
-    try {
-        root = YAML::LoadFile(std::string(filepath));
+	YAML::Node root;
+
+	try {
+		root = YAML::LoadFile(filepath);
 	} catch (const YAML::ParserException &e) {
-		std::cerr << "parse error: " << e.what() << "\n";
-		throw ParseError();
-    } catch (const std::exception& e) {
-		std::cerr << "error: " << e.what() << "\n";
-        return json{};
-    }
+		throw ParseError{};
+	} catch (const std::exception& e) {
+		return json{};
+	}
+
 	return detail::yaml2json(root);
 }
 
-/**
- * Load a file containing a xml document into a nlohmann::json object.
- */
-inline json loadxml(std::string_view filepath)
+/// \brief Load XML file to JSON.
+TOJSON_NODISCARD inline json loadxml(const std::string &filepath)
 {
 	std::ifstream file(filepath.data());
-	std::string str((std::istream_iterator<char>(file)), std::istream_iterator<char>());
+	std::string str((std::istream_iterator<char>(file)),
+									std::istream_iterator<char>());
 	return xml2json(str);
 }
 
 }
 
-#endif /* TOJSON_H */
+#endif /* TOJSON_HPP_ */
